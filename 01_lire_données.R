e858e4f2d595081cca0e66135b817b53ceebc3f7
TTRademacher <rademacher.tim@gmail.com>
2023-06-05 13:29:26 -0400

    J'ai intégré l'erreur de mesure.

--- 01_lire_données.R
+++ 01_lire_données.R
@@ -14,7 +14,11 @@ if(!existsFunction('%>%')) library('tidyverse')
 systeme <- c('A','A','B','B','C','C','E','E','E','E')
 traitement <- c('C','T','C','T','C','T','C','T','T','T')
 
-info <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+# initialiser le nom du fichier ------------------------------------------------
+file_name <- '../données/Compilation donnée érablière - 2023.xlsm'
+
+# extraire les metadonnées -----------------------------------------------------
+info <- readxl::read_excel(path = file_name,
                            sheet = 'Paramètres', range = 'B19:K19',
                            col_types = c(rep('numeric', 10)),
                            col_names = FALSE) %>% 
@@ -26,7 +30,7 @@ info <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
                         '#a6dba0','#008837','#a6dba0','#c2a5cf','#7b3294'),
              sym = c(21, 21, 22, 22, 23, 23, 24, 24, 24, 24))
 
-# Initialise column names and column types -------------------------------------
+# initialise column names and column types -------------------------------------
 column_names <- c(
   'date','time', 'compteur_1', 'coulee_m3_1', 'coulee_l_1', 'coulee_cum_1',
   'rendement_1', 'compteur_2', 'coulee_m3_2', 'coulee_l_2', 'coulee_cum_2',
@@ -36,7 +40,7 @@ column_names <- c(
 column_types <- c('date', 'text', rep('numeric', 17), 'text', rep('numeric', 6))
 
 # lire les données du système A contrôle (24" au dessus) -----------------------
-A_h2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+A_h2 <- readxl::read_excel(path = file_name,
                            sheet = '(1)', skip = 4, na = '-',
                            col_types = column_types, 
                            col_names = column_names) %>%
@@ -48,7 +52,7 @@ A_h2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'h2', systeme = 'A')
 
 # lire les données du système A traitement (24" en dessous) --------------------
-A_b2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+A_b2 <- readxl::read_excel(path = file_name,
                            sheet = '(2)', skip = 4, na = '-',
                            col_types = column_types,
                            col_names = column_names) %>%
@@ -60,7 +64,7 @@ A_b2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'b2', systeme = 'A')
 
 # lire les données du système B contrôle (4" en dessous) -----------------------
-B_b1 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+B_b1 <- readxl::read_excel(path = file_name,
                            sheet = '(3)', skip = 4, na = '-',
                            col_types = column_types,
                            col_names = column_names) %>%
@@ -72,7 +76,7 @@ B_b1 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'b1', systeme = 'B')
 
 # lire les données du système B traitement (24" en dessous) --------------------
-B_b2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+B_b2 <- readxl::read_excel(path = file_name,
                            sheet = '(4)', skip = 4, na = '-',
                            col_types = column_types,
                            col_names = column_names) %>%
@@ -84,7 +88,7 @@ B_b2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'b2', systeme = 'B')
 
 # lire les données du système C contrôle (24" au dessus) -----------------------
-C_h2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+C_h2 <- readxl::read_excel(path = file_name,
                            sheet = '(5)', skip = 4, na = '-',
                            col_types = column_types,
                            col_names = column_names) %>%
@@ -96,7 +100,7 @@ C_h2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'h2', systeme = 'C')
 
 # lire les données du système C traitement (4" au dessus) ----------------------
-C_h1 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+C_h1 <- readxl::read_excel(path = file_name,
                            sheet = '(6)', skip = 4, na = '-',
                            col_types = column_types,
                            col_names = column_names) %>%
@@ -108,7 +112,7 @@ C_h1 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'h1', systeme = 'C')
 
 # lire les données du système E contrôle (24" au dessus) -----------------------
-E_h2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+E_h2 <- readxl::read_excel(path = file_name,
                            sheet = '(7)', skip = 4, na = '-',
                            col_types = column_types[-c(13:18, 22)],
                            col_names = column_names[-c(13:18, 22)]) %>%
@@ -120,7 +124,7 @@ E_h2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'h2', systeme = 'E')
 
 # lire les données du système E traitement 1 (4" au dessus) --------------------
-E_h1 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+E_h1 <- readxl::read_excel(path = file_name,
                            sheet = '(8)', skip = 4, na = '-',
                            col_types = column_types[-c(13:18, 22)],
                            col_names = column_names[-c(13:18, 22)]) %>%
@@ -132,7 +136,7 @@ E_h1 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'h1', systeme = 'E')
 
 # lire les données du système E traitement 2 (4" en dessous) -------------------
-E_b1 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+E_b1 <- readxl::read_excel(path = file_name,
                            sheet = '(9)', skip = 4, na = '-',
                            col_types = column_types[-c(13:18, 22)],
                            col_names = column_names[-c(13:18, 22)]) %>%
@@ -144,7 +148,7 @@ E_b1 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
   add_column(h = 'b1', systeme = 'E')
 
 # lire les données du système E traitement 3 (24" en dessous) ------------------
-E_b2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.xlsm',
+E_b2 <- readxl::read_excel(path = file_name,
                            sheet = '(10)', skip = 4, na = '-',
                            col_types = column_types[-c(13:18, 22)],
                            col_names = column_names[-c(13:18, 22)]) %>%
@@ -157,11 +161,70 @@ E_b2 <- readxl::read_excel(path = '../Compilation donnée érablière - 2023.
 
 # combiner tous les données pertinentes ----------------------------------------
 d <- rbind(A_h2, A_b2, B_b1, B_b2, C_h2, C_h1, E_h2, E_h1, E_b1, E_b2) %>% 
-  select(systeme, h, date, rendement, brix) %>% filter(rendement > 0)
+  select(systeme, h, date, datetime, rendement, brix) %>% filter(rendement > 0)
 
 # supprime les lignes sans date, car elles sont les moyennes -------------------
 d <- d %>% filter(!is.na(date))
 
+# re-définir les noms des colonnes ---------------------------------------------
+column_names <- c('date', 'time', 'responsable', 'periode', 'A_h2', 'C_h2', 
+                  'C_h1', 'B_b1', 'A_b2', 'B_b2')
+
+# lire les données pour atp ----------------------------------------------------
+brix <- readxl::read_excel(path = '../données/Copie de 4010408_RécolteSN_V2JH.xlsx',
+                           sheet = 'CompilationDonnées', range = 'A4:J14',
+                           col_types = c('date', 'text', 'text', 'text', rep('numeric', 6)),
+                           col_names = column_names) %>% 
+  pivot_longer(cols = c('A_h2', 'C_h2', 'C_h1', 'B_b1', 'A_b2', 'B_b2'), 
+               names_to = c('systeme', 'h'), names_sep = '_') %>%
+  mutate(date = as_date(date),
+         heure = as.numeric(substr(time, 1, 2)),
+         minute = as.numeric(substr(time, 6, 7)),
+         datetime = ymd_hm(str_c(date, heure, minute, sep = " "))) %>%
+  select(-c(responsable, periode, time, heure, minute))
+atp <- readxl::read_excel(path = '../données/Copie de 4010408_RécolteSN_V2JH.xlsx',
+                          sheet = 'CompilationDonnées', range = 'A25:J35',
+                          col_types = c('date', 'text', 'text', 'text', rep('numeric', 6)),
+                          col_names = column_names) %>% 
+  pivot_longer(cols = c('A_h2', 'C_h2', 'C_h1', 'B_b1', 'A_b2', 'B_b2'), 
+               names_to = c('systeme', 'h'), names_sep = '_') %>%
+  mutate(date = as_date(date),
+         heure = as.numeric(substr(time, 1, 2)),
+         minute = as.numeric(substr(time, 6, 7)),
+         datetime = ymd_hm(str_c(date, heure, minute, sep = " "))) %>%
+  select(-c(responsable, periode, time, heure, minute))
+ph <-  readxl::read_excel(path = '../données/Copie de 4010408_RécolteSN_V2JH.xlsx',
+                          sheet = 'CompilationDonnées', range = 'A47:J57',
+                          col_types = c('date', 'text', 'text', 'text', rep('numeric', 6)),
+                          col_names = column_names) %>% 
+  pivot_longer(cols = c('A_h2', 'C_h2', 'C_h1', 'B_b1', 'A_b2', 'B_b2'), 
+               names_to = c('systeme', 'h'), names_sep = '_') %>%
+  mutate(date = as_date(date),
+         heure = as.numeric(substr(time, 1, 2)),
+         minute = as.numeric(substr(time, 6, 7)),
+         datetime = ymd_hm(str_c(date, heure, minute, sep = " "))) %>% 
+  select(-c(responsable, periode, time, heure, minute))
+sc <-  readxl::read_excel(path = '../données/Copie de 4010408_RécolteSN_V2JH.xlsx',
+                          sheet = 'CompilationDonnées', range = 'A92:J102',
+                          col_types = c('date', 'text', 'text', 'text', rep('numeric', 6)),
+                          col_names = column_names) %>% 
+  pivot_longer(cols = c('A_h2', 'C_h2', 'C_h1', 'B_b1', 'A_b2', 'B_b2'), 
+               names_to = c('systeme', 'h'), names_sep = '_') %>%
+  mutate(date = as_date(date),
+         heure = as.numeric(substr(time, 1, 2)),
+         minute = as.numeric(substr(time, 6, 7)),
+         datetime = ymd_hm(str_c(date, heure, minute, sep = " "))) %>%
+  select(-c(responsable, periode, time, heure, minute)) 
+
+# unir les données -------------------------------------------------------------
+tmp1 <- full_join(brix, atp, by = c('date', 'datetime', 'systeme', 'h')) %>% 
+  rename(brix = value.x, atp = value.y) %>%
+  relocate(systeme, h, date, datetime, brix, atp)
+tmp2 <- full_join(ph, sc, by = c('date', 'datetime', 'systeme', 'h')) %>% 
+  rename(ph = value.x, sc = value.y) %>%
+  relocate(systeme, h, date, datetime, ph, sc)
+d1 <- full_join(tmp1, tmp2, by = c('systeme', 'h', 'date', 'datetime'))
+
 # nettoyer l'espace de travail -------------------------------------------------
-rm(A_b2, A_h2, B_b1, B_b2, C_h1, C_h2, E_b1, E_b2, E_h1, E_h2, column_names, 
-   column_types, systeme, traitement)
+rm(A_b2, A_h2, atp, B_b1, B_b2, brix, C_h1, C_h2, column_names, column_types, 
+   E_b1, E_b2, E_h1, E_h2, file_name, ph, sc, systeme, tmp1, tmp2, traitement)
--- 02_analyse.R
+++ 02_analyse.R
@@ -6,35 +6,44 @@ if(!existsFunction('%>%')) library('tidyverse')
 # lire les données -------------------------------------------------------------
 if (!exists('d')) source('01_lire_données.R')
 
+# erreur des instruments -------------------------------------------------------
+atp_se   <- 5      # Hygiena SureSystem II 5% ou 5 RLUs
+ph_se    <- 0.01   # Atago Pal-Maple (0-85%)
+brix1_se <- 0.2    # Extech ExStick 
+
 # rendement par hauteur et systeme ---------------------------------------------
 par(mar = c(5, 5, 1, 2))
-plot(x = d$date, y = d$rendement, 
+plot(x = d$datetime, y = d$rendement, 
      xlab = 'Date', ylab = 'Rendement par entaille (litres)', axes = FALSE, 
-     col = 'white', ylim = c(0, 12))
-axis(side = 1, at = c(as_date('2023-03-20'), as_date('2023-03-27'), 
-                      as_date('2023-04-03'), as_date('2023-04-10'), 
-                      as_date('2023-04-17')),
+     col = 'white', xlim = as_datetime(c('2023-03-19', '2023-04-17')), ylim = c(0, 12))
+axis(side = 1, at = as_datetime(c('2023-03-20', '2023-03-27', '2023-04-03', 
+                                  '2023-04-10', '2023-04-17')),
      labels = c('20 mar', '27 mar', '3 avr', '10 avr', '17 avr'))
 axis(side = 2, las = 1)
 for (i in 1:dim(info)[1]){
   con <- d$h == info$h[i] & d$systeme == info$systeme[i]
-  points(d$date[con], y = d$rendement[con], pch = info$sym[i], col = info$colour[i], 
+  points(x = d$datetime[con] + 
+           hours(sample(-4:4, size = length(d$datetime[con]), replace = TRUE)), 
+         y = d$rendement[con], pch = info$sym[i], col = info$colour[i], 
          bg = info$colour[i])
 }
-legend(x = as_date('2023-04-12'), y = 12, bg = 'transparent', box.lty = 0,
-       legend = rep('',  4), col = c('#008837', 'white', 'white', '#7b3294'),
+legend(x = as_datetime('2023-04-11 12:00:00'), y = 12, bg = 'transparent', 
+       box.lty = 0, legend = rep('',  4), 
+       col = c('#008837', 'white', 'white', '#7b3294'),
        pt.bg = c('#008837', 'white', 'white', '#7b3294'),
        pch = 21, title = 'A')
-legend(x = as_date('2023-04-13'), y = 12, bg = 'transparent', box.lty = 0,
-       legend = rep('',  4), col = c('white', 'white', '#c2a5cf', '#7b3294'),
+legend(x = as_datetime('2023-04-12 12:00:00'), y = 12, bg = 'transparent', 
+       box.lty = 0, legend = rep('',  4), 
+       col = c('white', 'white', '#c2a5cf', '#7b3294'),
        pt.bg = c('white', 'white', '#c2a5cf', '#7b3294'),
        pch = 22, title = 'B')
-legend(x = as_date('2023-04-14'), y = 12, bg = 'transparent', box.lty = 0,
-       legend = rep('',  4), col = c('#008837', '#a6dba0', 'white', 'white'),
+legend(x = as_datetime('2023-04-13 12:00:00'), y = 12, bg = 'transparent', 
+       box.lty = 0, legend = rep('',  4), 
+       col = c('#008837', '#a6dba0', 'white', 'white'),
        pt.bg = c('#008837', '#a6dba0', 'white', 'white'),
        pch = 23, title = 'C')
-legend(x = as_date('2023-04-15'), y = 12, bg = 'transparent', box.lty = 0,
-       legend = c('+24"',  '  +4"', '   -4"', ' -24"'), 
+legend(x = as_datetime('2023-04-14 12:00:00'), y = 12, bg = 'transparent', 
+       box.lty = 0, legend = c('+24"',  '  +4"', '   -4"', ' -24"'), 
        col = c('#008837', '#a6dba0', '#c2a5cf', '#7b3294'),
        pt.bg = c('#008837', '#a6dba0', '#c2a5cf', '#7b3294'),
        pch = 24, title = 'E')
@@ -55,77 +64,204 @@ mod_vol <- brms::brm(brms::bf(rendement ~
                       seed = 1353,
                       backend = 'cmdstanr')
 
-# additional posterior distribution checks -------------------------------------
+# vérifier la distribution postérieur ------------------------------------------
 plot(mod_vol)
 pp_check(mod_vol, ndraws = 100)
 pp_check(mod_vol, type = 'error_hist',  ndraws = 10)
 pp_check(mod_vol, type = 'scatter_avg', ndraws = 100)
-# Error in the posterior distribution looks normally-distributed
+# erreur de la distribution postérieur semble être distribuée normalement
 
-# get model summary and coefficients -------------------------------------------
+# regarder le sommaire et les coéfficients -------------------------------------
 summary(mod_vol)
 ranef(mod_vol)$systeme [, , 'Intercept']
 ranef(mod_vol)$date [, , 'Intercept']
 
-# rendement par hauteur et systeme ---------------------------------------------
+# teneur en sucre par hauteur et système ---------------------------------------
 par(mar = c(5, 5, 1, 2))
-plot(x = d$date, y = d$brix, 
+plot(x = d$datetime, y = d$brix, 
      xlab = 'Date', 
      ylab = expression(paste('Teneur en sucre (',degree,'Brix)', sep = '')), 
      axes = FALSE, 
-     col = 'white', ylim = c(0, 4))
-axis(side = 1, at = c(as_date('2023-03-20'), as_date('2023-03-27'), 
-                      as_date('2023-04-03'), as_date('2023-04-10'), 
-                      as_date('2023-04-17')),
+     col = 'white', 
+     xlim = as_datetime(c('2023-03-19', '2023-04-17')), ylim = c(0, 4))
+axis(side = 1, at = as_datetime(c('2023-03-20', '2023-03-27', '2023-04-03', 
+                                  '2023-04-10', '2023-04-17')),
      labels = c('20 mar', '27 mar', '3 avr', '10 avr', '17 avr'))
 axis(side = 2, las = 1)
 for (i in 1:dim(info)[1]){
   con <- d$h == info$h[i] & d$systeme == info$systeme[i]
-  points(d$date[con], y = d$brix[con], pch = info$sym[i], col = info$colour[i], 
+  points(x = d$datetime[con] + 
+           hours(sample(-4:4, size = length(d$datetime[con]), replace = TRUE)), 
+         y = d$brix[con], pch = info$sym[i], col = info$colour[i], 
          bg = info$colour[i])
 }
-legend(x = as_date('2023-04-12'), y = 4, bg = 'transparent', box.lty = 0,
-       legend = rep('',  4), col = c('#008837', 'white', 'white', '#7b3294'),
+legend(x = as_datetime('2023-04-12 12:00:00'), y = 4, bg = 'transparent', 
+       box.lty = 0, legend = rep('',  4), 
+       col = c('#008837', 'white', 'white', '#7b3294'),
        pt.bg = c('#008837', 'white', 'white', '#7b3294'),
        pch = 21, title = 'A')
-legend(x = as_date('2023-04-13'), y = 4, bg = 'transparent', box.lty = 0,
-       legend = rep('',  4), col = c('white', 'white', '#c2a5cf', '#7b3294'),
+legend(x = as_datetime('2023-04-13 12:00:00'), y = 4, bg = 'transparent', 
+       box.lty = 0, legend = rep('',  4), 
+       col = c('white', 'white', '#c2a5cf', '#7b3294'),
        pt.bg = c('white', 'white', '#c2a5cf', '#7b3294'),
        pch = 22, title = 'B')
-legend(x = as_date('2023-04-14'), y = 4, bg = 'transparent', box.lty = 0,
-       legend = rep('',  4), col = c('#008837', '#a6dba0', 'white', 'white'),
+legend(x = as_datetime('2023-04-14 12:00:00'), y = 4, bg = 'transparent', 
+       box.lty = 0, legend = rep('',  4), 
+       col = c('#008837', '#a6dba0', 'white', 'white'),
        pt.bg = c('#008837', '#a6dba0', 'white', 'white'),
        pch = 23, title = 'C')
-legend(x = as_date('2023-04-15'), y = 4, bg = 'transparent', box.lty = 0,
-       legend = c('+24"',  '  +4"', '   -4"', ' -24"'), 
+legend(x = as_datetime('2023-04-15 12:00:00'), y = 4, bg = 'transparent', 
+       box.lty = 0,
+       legend = c('+24\"',  '  +4"', '   -4"', ' -24"'), 
        col = c('#008837', '#a6dba0', '#c2a5cf', '#7b3294'),
        pt.bg = c('#008837', '#a6dba0', '#c2a5cf', '#7b3294'),
        pch = 24, title = 'E')
 
-# L'effet de l'hauteur de l'entaille sur le rendement --------------------------
-mod_brix <- brms::brm(brms::bf(brix ~ 
+# effet de l'hauteur de l'entaille sur le teneur en sucre ----------------------
+mod_brix1 <- brms::brm(brms::bf(brix | mi(brix1_se) ~ 
                              h +             # hauteur de l'entaille 
                              (1 | date) +    # différence par date
                              (1 | systeme)), # difference antre systèmes
-                  data = d,
-                  family = gaussian(), 
-                  prior = c(set_prior('normal(2, 10)', class = 'Intercept'),
-                            set_prior('exponential(1)', class = 'sigma'),
-                            set_prior('normal(0, 2)', class = 'b')),
-                  cores = 4, chains = 4,
-                  control = list(adapt_delta = 0.99),
-                  iter = 6000,
-                  seed = 1353,
-                  backend = 'cmdstanr')
+                       data = d %>% add_column(brix1_se = brix1_se) %>% 
+                         select(-rendement, -datetime),
+                       family = gaussian(), 
+                       prior = c(set_prior('normal(2, 5)', class = 'Intercept'),
+                                 set_prior('exponential(1)', class = 'sigma'),
+                                 set_prior('normal(0, 2)', class = 'b')),
+                       cores = 4, chains = 4,
+                       control = list(adapt_delta = 0.99),
+                       iter = 6000,
+                       seed = 1353,
+                       backend = 'cmdstanr')
 
-# additional posterior distribution checks -------------------------------------
-plot(mod_brix)
-pp_check(mod_brix, ndraws = 100)
-pp_check(mod_brix, type = 'error_hist',  ndraws = 10)
-pp_check(mod_brix, type = 'scatter_avg', ndraws = 100)
-# Error in the posterior distribution looks normally-distributed
+# vérifier la distribution postérieur ------------------------------------------
+plot(mod_brix1)
+pp_check(mod_brix1, ndraws = 100)
+pp_check(mod_brix1, type = 'error_hist',  ndraws = 10)
+pp_check(mod_brix1, type = 'scatter_avg', ndraws = 100)
+# erreur de la distribution postérieur semble être distribuée normalement
 
-# get model summary and coefficients -------------------------------------------
-summary(mod_brix)
-ranef(mod_brix)$systeme [, , 'Intercept']
-ranef(mod_brix)$date [, , 'Intercept']
+# regarder le sommaire et les coéfficients -------------------------------------
+summary(mod_brix1)
+ranef(mod_brix1)$systeme [, , 'Intercept']
+ranef(mod_brix1)$date [, , 'Intercept']
+
+# effet de l'hauteur de l'entaille sur le teneur en sucre ----------------------
+mod_brix2 <- brms::brm(brms::bf(brix ~ 
+                                 h +             # hauteur de l'entaille 
+                                 (1 | date) +    # différence par date
+                                 (1 | systeme)), # difference antre systèmes
+                       data = d1,
+                       family = gaussian(), 
+                       prior = c(set_prior('normal(2, 10)', class = 'Intercept'),
+                                 set_prior('exponential(1)', class = 'sigma'),
+                                 set_prior('normal(0, 2)', class = 'b')),
+                       cores = 4, chains = 4,
+                       control = list(adapt_delta = 0.9),
+                       iter = 6000,
+                       seed = 1353,
+                       backend = 'cmdstanr')
+
+# vérifier la distribution postérieur ------------------------------------------
+plot(mod_brix2)
+pp_check(mod_brix2, ndraws = 100)
+pp_check(mod_brix2, type = 'error_hist',  ndraws = 10)
+pp_check(mod_brix2, type = 'scatter_avg', ndraws = 100)
+# erreur de la distribution postérieur semble être distribuée normalement
+
+# regarder le sommaire et les coéfficients -------------------------------------
+summary(mod_brix2)
+ranef(mod_brix2)$systeme [, , 'Intercept']
+ranef(mod_brix2)$date [, , 'Intercept']
+
+# effet de l'hauteur de l'entaille sur la contamination microbienne ------------
+# TR - Need to look at whether a lognormal distribution isr really the best fit.
+# It almost looks uniformly distributed.
+mod_atp <- brms::brm(brms::bf(log(atp) ~ 
+                                  h +             # hauteur de l'entaille 
+                                  (1 | date) +    # différence par date
+                                  (1 | systeme)), # difference antre systèmes
+                     data = d1,
+                     family = gaussian(), 
+                     prior = c(#set_prior('normal(2, 10)', class = 'Intercept'),
+                               set_prior('exponential(1)', class = 'sigma'),
+                               set_prior('normal(0, 2)', class = 'b')),
+                     cores = 4, chains = 4,
+                     control = list(adapt_delta = 0.9),
+                     iter = 6000,
+                     seed = 1353,
+                     backend = 'cmdstanr')
+
+# vérifier la distribution postérieur ------------------------------------------
+plot(mod_atp)
+pp_check(mod_atp, ndraws = 100)
+pp_check(mod_atp, type = 'error_hist',  ndraws = 10)
+pp_check(mod_atp, type = 'scatter_avg', ndraws = 100)
+# erreur de la distribution postérieur semble être distribuée normalement
+
+# regarder le sommaire et les coéfficients -------------------------------------
+summary(mod_atp)
+ranef(mod_atp)$systeme [, , 'Intercept']
+ranef(mod_atp)$date [, , 'Intercept']
+
+# effet de l'hauteur de l'entaille sur le pH -----------------------------------
+mod_ph <- brms::brm(brms::bf(ph | mi(ph_se) ~ 
+                               h +             # hauteur de l'entaille 
+                               (1 | date) +    # différence par date
+                               (1 | systeme)), # difference antre systèmes
+                    data = d1 %>% select(-c(brix, datetime, atp, sc)) %>% 
+                      add_column(ph_se = ph_se),
+                    family = gaussian(), 
+                    prior = c(set_prior('normal(2, 10)', class = 'Intercept'),
+                              set_prior('exponential(1)', class = 'sigma'),
+                              set_prior('normal(0, 2)', class = 'b')),
+                    cores = 4, chains = 4,
+                    control = list(adapt_delta = 0.99),
+                    iter = 6000,
+                    seed = 1353,
+                    backend = 'cmdstanr')
+
+# vérifier la distribution postérieur ------------------------------------------
+plot(mod_ph)
+pp_check(mod_ph, ndraws = 100)
+pp_check(mod_ph, type = 'error_hist',  ndraws = 10)
+pp_check(mod_ph, type = 'scatter_avg', ndraws = 100)
+# erreur de la distribution postérieur semble être distribuée normalement, mais 
+# il n'y a pas de données de ph entre 6,4 et 7,0, ce que semble bizarre
+
+# regarder le sommaire et les coéfficients -------------------------------------
+summary(mod_ph)
+ranef(mod_ph)$systeme [, , 'Intercept']
+ranef(mod_ph)$date [, , 'Intercept']
+
+# effet de l'hauteur de l'entaille sur le pH -----------------------------------
+mod_sc <- brms::brm(brms::bf(sc ~ 
+                               h +             # hauteur de l'entaille 
+                               (1 | date) +    # différence par date
+                               (1 | systeme)), # difference antre systèmes
+                    data = d1,
+                    family = gaussian(), 
+                    prior = c(set_prior('normal(2, 10)', class = 'Intercept'),
+                              set_prior('exponential(1)', class = 'sigma'),
+                              set_prior('normal(0, 2)', class = 'b')),
+                    cores = 4, chains = 4,
+                    control = list(adapt_delta = 0.99),
+                    iter = 6000,
+                    seed = 1353,
+                    backend = 'cmdstanr')
+
+# vérifier la distribution postérieur ------------------------------------------
+plot(mod_sc)
+pp_check(mod_sc, ndraws = 100)
+pp_check(mod_sc, type = 'error_hist',  ndraws = 10)
+pp_check(mod_sc, type = 'scatter_avg', ndraws = 100)
+# erreur de la distribution postérieur semble être distribuée normalement, mais 
+# Pourquoi la distribution est bi-modal. Il va falloir que je refait l'analyse 
+# une fois que les données de Mustafa ont été corrigé
+
+# regarder le sommaire et les coéfficients -------------------------------------
+summary(mod_sc)
+ranef(mod_sc)$systeme [, , 'Intercept']
+ranef(mod_sc)$date [, , 'Intercept']
+# Il semble avoir approximativement le même effet que avec le brix mesuré avec 
+# un refractomètre
